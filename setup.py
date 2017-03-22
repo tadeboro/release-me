80cc9d2f2d46e1d0b9766f8c4dcefae5c03ce9ef
Tadej Borovšak <tadeboro@gmail.com>
2017-03-22 20:58:50 +0100

    Use pbr for simpler setup
    Using pbr package makes maintaining the releases much easier and less
    error prone.

--- setup.py
+++ setup.py
@@ -13,23 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import setuptools
+from setuptools import setup
 
-
-setuptools.setup(
-    name="release-me",
-    version="0.1.0",
-    author="tadeboro",
-    author_email="tadeboro@gmail.com",
-    packages=["release_me"],
-    license="LICENSE",
-    description="Simple Github CLI release helper",
-    install_requires=[
-        "requests>=2.4.2",
-    ],
-    entry_points={
-        "console_scripts": [
-            "github-release = release_me.cli:main",
-        ]
-    }
-)
+setup(setup_requires=["pbr>=2.0.0"], pbr=True)
