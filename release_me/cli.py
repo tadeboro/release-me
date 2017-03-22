2bc149b529dd4aecc53bff5f5d028df2051162fd
Tadej Borovšak <tadeboro@gmail.com>
2017-03-22 21:00:07 +0100

    Rework the client and command line tool
    Code has not been ready to receive update that would allow us to
    upload assets to release, so we changed it a bit. Main difference is
    explicit URL passing to all API calls, which makes it much easier to
    reason about the call being made.
    
    Another additions is use of keyword arguments in all wrappers for GET,
    POST and DELETE calls that makes it possible to pass arbitrary
    additional arguments to requests.

--- release_me/cli.py
+++ release_me/cli.py
@@ -15,96 +15,160 @@
 
 from __future__ import print_function
 
-from . import client
-
-import pkg_resources
 import argparse
 import json
+import logging
+import os
 import sys
 
+from release_me.client import GithubClient
 
-def get_client(token):
+
+class ArgParser(argparse.ArgumentParser):
+    """
+    Argument parser that displays help on error
+    """
+
+    def error(self, message):
+        sys.stderr.write("error: {}\n".format(message))
+        self.print_help()
+        sys.exit(2)
+
+    def add_subparsers(self):
+        # Workaround for http://bugs.python.org/issue9253
+        subparsers = super(ArgParser, self).add_subparsers()
+        subparsers.required = True
+        subparsers.dest = "command"
+        return subparsers
+
+
+def _get_client(logger):
     try:
-        c = client.GithubClient(token)
-    except client.GithubClientError as e:
-        print("ERROR: {}".format(e.msg))
+        token = os.environ["RELEASEME_TOKEN"]
+        client = GithubClient(token)
+    except client.GithubClientError as error:
+        logger.error(error)
         sys.exit(1)
-    return c
+    except KeyError:
+        logger.error("Missing access token. Set RELEASEME_TOKEN env variable.")
+        sys.exit(1)
+    return client
 
 
-def check_repo(c, repo):
-    repo_exists, _ = c.get_repo(repo)
+def check_repo(client, logger, repo):
+    repo_exists, _ = client.get_repo(repo)
     if not repo_exists:
-        print("ERROR: invalid repo '{}'".format(args.repo))
+        logger.error("Invalid repository '{}'".format(repo))
         sys.exit(1)
 
 
-def create_release(args):
+def create_release(client, logger, args):
     """
     Create new Github release.
     """
-    c = get_client(args.token)
-    check_repo(c, args.repo)
+    check_repo(client, logger, args.repo)
 
+    logger.info("Creating release for tag {} {}".format(args.repo, args.tag))
     notes = args.notes.read()
-    created, r = c.create_release(args.repo, args.tag, notes)
+    created, response = client.create_release(args.repo, args.tag, notes)
     if not created:
-        print("Error:\n'{}'".format(json.dumps(r, indent=2)))
+        logger.error(response)
         sys.exit(1)
 
+    release_id = response["id"]
+    assets = [] if args.asset is None else args.asset
+    for asset in assets:
+        logger.info("Uploading asset {} for {}".format(asset, release_id))
+        success, response = client.upload_asset(args.repo, release_id, asset)
+        if not success:
+            logger.error(response)
 
-def delete_release(args):
+
+def delete_release(client, logger, args):
     """
     Delete Github release
     """
-    c = get_client(args.token)
-    check_repo(c, args.repo)
-    success, release = c.get_release(args.repo, args.tag)
+    check_repo(client, logger, args.repo)
+
+    logger.info("Getting release for tag {}".format(args.tag))
+    success, release = client.get_release(args.repo, args.tag)
     if not success:
-        print("Error: release '{}' not found.".format(args.tag))
+        logger.error("Release {} {} not found.".format(args.repo, args.tag))
         sys.exit(1)
 
-    deleted, r = c.delete_release(args.repo, release["id"])
-    print(json.dumps(release, indent=2))
+    logger.info("Deleting release for tag {}".format(args.tag))
+    deleted, response = client.delete_release(args.repo, release["id"])
     if not deleted:
-        print("Error:\n'{}'".format(json.dumps(r, indent=2)))
+        logger.error(response)
         sys.exit(1)
 
 
-def add_common_params(parser):
-    parser.add_argument("--token", required=True,
-                        help="Github OAuth token")
-    parser.add_argument("--repo", required=True,
+def get_release(client, logger, args):
+    """
+    Get Github release for selected tag
+    """
+    check_repo(client, logger, args.repo)
+
+    logger.info("Getting release for tag {}".format(args.tag))
+    success, release = client.get_release(args.repo, args.tag)
+    if not success:
+        logger.error("Release {} {} not found.".format(args.repo, args.tag))
+        sys.exit(1)
+    print(
+        json.dumps(release, indent=2, separators=(",", ": "), sort_keys=True)
+    )
+
+
+def _add_common_params(parser):
+    parser.add_argument("-r", "--repo", required=True,
                         help="Github repo (eg. tadeboro/release-me)")
-    parser.add_argument("--tag", required=True,
+    parser.add_argument("-t", "--tag", required=True,
                         help="Existing git tag name")
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(description="Github release helper")
-    parser.add_argument(
-        "--version", action="version",
-        version=pkg_resources.get_distribution("release_me").version
-    )
-
+def _create_parser():
+    parser = ArgParser(description="Github release helper",
+                       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     subparsers = parser.add_subparsers()
 
-    create_p = subparsers.add_parser("create",
-                                     description=create_release.__doc__)
-    create_p.add_argument("--notes", required=True,
+    get_p = subparsers.add_parser("get", help="Get existing release")
+    get_p.set_defaults(func=get_release)
+    _add_common_params(get_p)
+
+    create_p = subparsers.add_parser("create", help="Create new release")
+    create_p.add_argument("-n", "--notes", required=True,
                           type=argparse.FileType("r"),
                           help="Path to release notes file")
+    create_p.add_argument("-a", "--asset", help="Asset to upload",
+                          action="append", default=[])
     create_p.set_defaults(func=create_release)
-    add_common_params(create_p)
+    _add_common_params(create_p)
 
-    delete_p = subparsers.add_parser("delete")
+    delete_p = subparsers.add_parser("delete", help="Delete release")
     delete_p.set_defaults(func=delete_release)
-    add_common_params(delete_p)
+    _add_common_params(delete_p)
 
     return parser
 
 
+def _setup_logging():
+    fmt_stream = logging.Formatter("[%(levelname)s] - %(message)s")
+    handler_stream = logging.StreamHandler()
+    handler_stream.setFormatter(fmt_stream)
+    handler_stream.setLevel(logging.INFO)
+
+    log = logging.getLogger("fcoclient")
+    log.addHandler(handler_stream)
+    log.setLevel(logging.DEBUG)
+
+    return log
+
+
 def main():
-    parser = create_parser()
+    parser = _create_parser()
     args = parser.parse_args()
-    args.func(args)
+
+    logger = _setup_logging()
+    client = _get_client(logger)
+
+    args.func(client, logger, args)
--- release_me/client.py
+++ release_me/client.py
@@ -13,25 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import mimetypes
+import os
+
 import requests
-import logging
-
-
-fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-fh = logging.FileHandler("tmp.log")
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(fmt)
-
-logger = logging.getLogger(__name__)
-logger.addHandler(fh)
-logger.setLevel(logging.DEBUG)
 
 
 class GithubClientError(Exception):
 
     def __init__(self, msg):
         self.msg = msg
+        super(GithubClientError, self).__init__(msg)
 
     def __str__(self):
         return "GithubClientError: {}".format(self.msg)
@@ -42,12 +34,10 @@ class GithubClient:
     Github client
     """
 
-    ENDPOINT = "https://api.github.com"
-    RELEASES_TMPL = "repos/{}/releases"
-    RELEASE_TMPL = "repos/{}/releases/{}"
+    API_ENDPOINT = "https://api.github.com"
+    UPLOAD_ENDPOINT = "https://uploads.github.com"
 
     def __init__(self, token):
-        logger.debug("Initializing Github client")
         self.headers = {
             "User-Agent": "tadeboro",
             "Authorization": "Token {}".format(token),
@@ -55,33 +45,30 @@ class GithubClient:
         self._validate_token()
 
     def _validate_token(self):
-        logger.debug("Validating OAuth token")
-        if self._get().status_code != 200:
-            logger.error("Invalid token")
+        if self._get(self.API_ENDPOINT).status_code != 200:
             raise GithubClientError("Invalid token")
 
-    def _do_request(self, method, path, **kwargs):
-        url = "{}/{}".format(self.ENDPOINT, path)
-        return method(url, headers=self.headers, **kwargs)
+    def _do_request(self, method, url, **kwargs):
+        headers = kwargs.pop("headers", {})
+        headers.update(self.headers)
+        return method(url, headers=headers, **kwargs)
 
-    def _get(self, path=""):
-        logger.debug("GET '{}'".format(path))
-        return self._do_request(requests.get, path)
+    def _get(self, url, **kwargs):
+        return self._do_request(requests.get, url, **kwargs)
 
-    def _post(self, path, data):
-        logger.debug("POST '{}' ({})".format(path, data))
-        return self._do_request(requests.post, path, json=data)
+    def _post(self, url, **kwargs):
+        return self._do_request(requests.post, url, **kwargs)
 
-    def _delete(self, path):
-        logger.debug("DELETE '{}'".format(path))
-        return self._do_request(requests.delete, path)
+    def _delete(self, url, **kwargs):
+        return self._do_request(requests.delete, url, **kwargs)
 
     def get_repo(self, repo):
-        resp = self._get("repos/{}".format(repo))
+        resp = self._get("{}/repos/{}".format(self.API_ENDPOINT, repo))
         return resp.status_code == 200, resp.json()
 
     def get_release(self, repo, tag):
-        resp = self._get("repos/{}/releases/tags/{}".format(repo, tag))
+        url = "{}/repos/{}/releases/tags/{}"
+        resp = self._get(url.format(self.API_ENDPOINT, repo, tag))
         return resp.status_code == 200, resp.json()
 
     def create_release(self, repo, tag, body):
@@ -90,10 +77,25 @@ class GithubClient:
             "name": tag,
             "body": body,
         }
-        resp = self._post("repos/{}/releases".format(repo), data)
+        url = "{}/repos/{}/releases"
+        resp = self._post(url.format(self.API_ENDPOINT, repo), json=data)
         return resp.status_code == 201, resp.json()
 
-    def delete_release(self, repo, id):
-        resp = self._delete("repos/{}/releases/{}".format(repo, id))
+    def delete_release(self, repo, release_id):
+        url = "{}/repos/{}/releases/{}"
+        resp = self._delete(url.format(self.API_ENDPOINT, repo, release_id))
         success = resp.status_code == 204
-        return success, {} if success else resp.json()
+        return success, {} if success else resp.text
+
+    def upload_asset(self, repo, release_id, asset):
+        content_type, _ = mimetypes.guess_type(asset)
+        if content_type is None:
+            content_type = "application/octet-stream"
+
+        with open(asset, "rb") as handle:
+            data = handle.read()
+        url = "{}/repos/{}/releases/{}/assets"
+        resp = self._post(url.format(self.UPLOAD_ENDPOINT, repo, release_id),
+                          data=data, headers={"Content-Type": content_type},
+                          params={"name": os.path.basename(asset)})
+        return resp.status_code == 201, resp.json()
